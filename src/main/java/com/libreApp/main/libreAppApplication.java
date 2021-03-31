173d327171e8b25fadc494aac537a0e9a05dba87
Juan García Hernández <juan@juan>
2021-03-31 21:35:50 +0200

    Añadida estructura inicial del proyecto para Maven con Spring

---
+++ src/main/java/com/libreApp/main/libreAppApplication.java
@@ -0,0 +1,13 @@
+package com.libreApp.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class libreAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(libreAppApplication.class, args);
+	}
+
+}
