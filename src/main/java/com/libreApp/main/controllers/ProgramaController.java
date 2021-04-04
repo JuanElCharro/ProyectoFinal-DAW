57ad3a48f74dc599f52a0d78a958eaf4eb14d46e
Juan García Hernández <juan@juan>
2021-04-05 00:06:42 +0200

    Añadidos JSP y Controlador de Programa (vacíos). Arreglado un bug que mostraba mal el menú

---
+++ src/main/java/com/libreApp/main/controllers/ProgramaController.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.libreApp.main.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Juan
+ *
+ */
+
+@Controller
+public class ProgramaController {
+	
+}
