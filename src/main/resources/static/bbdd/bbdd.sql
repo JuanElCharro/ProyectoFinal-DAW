38006408c5fe1facbfecc6481905f6f54ae6b965
Juan García Hernández <juan@juan>
2021-05-05 23:19:22 +0200

    Añadido formulario a Crear Programa y añadidos datos al sql

--- src/main/resources/static/bbdd/bbdd.sql
+++ src/main/resources/static/bbdd/bbdd.sql
@@ -5,7 +5,7 @@ use libreapp;
 DROP TABLE IF EXISTS `categorias`;
 
 CREATE TABLE `libreapp`.`categorias` (
-  `id_categoria` INT NOT NULL,
+  `id_categoria` INT NOT NULL AUTO_INCREMENT,
   `nombre_categoria` VARCHAR(45) NULL,
   PRIMARY KEY (`id_categoria`)
   )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -13,7 +13,7 @@ CREATE TABLE `libreapp`.`categorias` (
 DROP TABLE IF EXISTS `licencias`;
 
 CREATE TABLE `libreapp`.`licencias` (
-  `id_licencia` INT NOT NULL,
+  `id_licencia` INT NOT NULL AUTO_INCREMENT,
   `nombre_licencia` VARCHAR(45) NULL,
   `descripcion` VARCHAR(500) NULL,
   PRIMARY KEY (`id_licencia`)
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `comentarios`;
 
 CREATE TABLE `libreapp`.`comentarios` (
   `id_comentario` INT NOT NULL,
-  `comentario` VARCHAR(45) NULL,
+  `comentario` VARCHAR(500) NULL,
   PRIMARY KEY (`id_comentario`)
   )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -38,7 +38,7 @@ CREATE TABLE `libreapp`.`aplicaciones` (
   `link_descarga` VARCHAR(100) NOT NULL,
   `descripcion` VARCHAR(500) NOT NULL,
   `id_categoria` INT NOT NULL,
-  `id_licencia` INT NULL,
+  `id_licencia` INT NOT NULL,
   `id_comentario` INT NULL,
   PRIMARY KEY (`id_app`),
   CONSTRAINT `id_categoria` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id_categoria`),
@@ -57,6 +57,16 @@ INSERT INTO `libreapp`.`categorias` (`id_categoria`, `nombre_categoria`) VALUES 
 INSERT INTO `libreapp`.`categorias` (`id_categoria`, `nombre_categoria`) VALUES ('9', 'Internet');
 INSERT INTO `libreapp`.`categorias` (`id_categoria`, `nombre_categoria`) VALUES ('10', 'Ciencia');
   
+INSERT INTO `libreapp`.`licencias` (`id_licencia`, `nombre_licencia`, `descripcion`) VALUES ('1', 'GPLv2', 'https://opensource.org/licenses/gpl-2.0.php');
+INSERT INTO `libreapp`.`licencias` (`id_licencia`, `nombre_licencia`, `descripcion`) VALUES ('2', 'GPLv3', 'https://www.gnu.org/licenses/gpl-3.0.en.html');
+INSERT INTO `libreapp`.`licencias` (`id_licencia`, `nombre_licencia`, `descripcion`) VALUES ('3', 'Apache License', 'https://apache.org/licenses/LICENSE-2.0.txt');
+INSERT INTO `libreapp`.`licencias` (`id_licencia`, `nombre_licencia`, `descripcion`) VALUES ('4', 'Mozilla Public License', 'https://www.mozilla.org/en-US/MPL/2.0/');
+INSERT INTO `libreapp`.`licencias` (`id_licencia`, `nombre_licencia`, `descripcion`) VALUES ('5', 'MIT license', 'https://mit-license.org/');
+
+INSERT INTO `libreapp`.`aplicaciones` (`id_app`, `nombre_app`, `version`, `fecha`, `link_imagen`, `link_descarga`, `descripcion`, `id_categoria`, `id_licencia`) VALUES ('1', 'LibreOffice', '7.0.5', '2021/05/05', 'https://image.flaticon.com/icons/png/512/23/23666.png', 'https://es.libreoffice.org/descarga/libreoffice/', 'LibreOffice es una suite ofimática completa compuesta de procesador de textos, hoja de cálculo, presentaciones, programa de dibujo, base de datos y editor de ecuaciones.', '2', '2');
+INSERT INTO `libreapp`.`aplicaciones` (`id_app`, `nombre_app`, `version`, `fecha`, `link_imagen`, `link_descarga`, `descripcion`, `id_categoria`, `id_licencia`) VALUES ('2', 'Code::Blocks', '20.03', '2020/04/03', 'https://www.codeblocks.org/images/logo160.png', 'http://www.codeblocks.org/downloads/binaries/', 'Code::Blocks es un IDE (entorno de desarrollo integrado) de C/C++, que permite utilizar varios compiladores a elección del usuario (GNU GCC, MinGW GCC, MS Visual C++, clang, Borland C++ 5.5, Open Watcom, etc., que deben instalarse por separado). Esta versión incluye MinGW-W64.', '1', '2');
+
+
 SET @@global.time_zone = '+01:00';
 SELECT @@global.time_zone;
 use libreapp;
--- src/main/webapp/WEB-INF/jsp/vistas/programas/crearPrograma.jsp
+++ src/main/webapp/WEB-INF/jsp/vistas/programas/crearPrograma.jsp
@@ -12,18 +12,85 @@
 
 		<!-- CSS -->
 		<link rel="stylesheet" href="css/menu.css">
-		<link rel="stylesheet" href="css/formularios.css">
 
 	</head>
 
 	<body>
 		<%@include file="../../menu.html" %>
 
-		<div class="container" style="margin-top:30px">
-			<h1>Crear Programa</h1>
-		</div>
+			<div class="jumbotron jumbotron-fluid">
+				<div class="container">
+					<h1 class="display-4">Crear entrada de App</h1>
+					<p class="lead">Usted puede crear un nuevo registro de un programa desde esta pantalla.
+					</p>
+				</div>
+			</div>
 
-		<%@include file="../../footer.html" %>
+			<div class="container" style="margin-top:30px">
+				<div class="col-12">
+					<div class="form-group">
+						<form action="http://localhost:8080/libreApp/crearPrograma" method="post" id="formulario">
+
+							<div class="row">
+								<div class="col-6">
+									<label for="id_app">Id:</label>
+									<input type="text" class="form-control" id="id_app" name="id_app">
+								</div>
+								<div class="col-6">
+									<label for="nombre_app">Nombre:</label>
+									<input type="text" class="form-control" id="nombre_app" name="nombre_app">
+								</div>
+							</div>
+
+							<div class="row">
+								<div class="col-6">
+									<label for="version">Version del programa:</label>
+									<input type="text" class="form-control" id="version" name="version">
+								</div>
+								<div class="col-6">
+									<label for="fecha">Fecha ultima version:</label>
+									<input type="date" class="form-control" id="fecha" name="fecha">
+								</div>
+							</div>
+
+							<label for="link_imagen">Link de la imagen:</label>
+							<input type="text" class="form-control" id="link_imagen" name="link_imagen">
+							<label for="link_descarga">Link de descarga:</label>
+							<input type="text" class="form-control" id="link_descarga" name="link_descarga">
+
+							<div class="form-group">
+								<label for="descripcion">Descripcion:</label>
+								<textarea class="form-control" id="descripcion" rows="3"></textarea>
+							</div>
+
+							<div class="row">
+								<div class="col-6">Categoria de la App:
+									<select class="form-control" name="categorias" form="formulario">
+										<c:forEach items="${listaCategorias}" var="categorias">
+											<option value="${categorias.id}"> ${categorias.nombre} </option>
+										</c:forEach>
+									</select>
+								</div>
+								<div class="col-6">Tipo de licencia:
+									<select class="form-control" name="licencias" form="formulario">
+										<c:forEach items="${listaLicencias}" var="licencias">
+											<option value="${licencias.id}"> ${licencias.descripcion} </option>
+										</c:forEach>
+									</select>
+								</div>
+							</div>
+							<br>
+							<input type="submit" class="btn btn-primary col-12" value="Guardar registro">
+							<hr>
+							<c:if test="${resultado == 1}">
+								<h2>App insertada correctamente</h2>
+							</c:if>
+					</div>
+				</div>
+
+			</div>
+
+			<%@include file="../../footer.html" %>
 	</body>
 
 	</html>
