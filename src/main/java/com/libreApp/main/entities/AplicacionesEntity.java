c99685ef8535d3a270a918fc3144aec562fe345b
Juan García Hernández <juan@juan>
2021-05-13 16:02:21 +0200

    Solucionado bug en la opción de insertar, ahora funciona correctamente

--- src/main/java/com/libreApp/main/entities/AplicacionesEntity.java
+++ src/main/java/com/libreApp/main/entities/AplicacionesEntity.java
@@ -7,6 +7,8 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -22,6 +24,7 @@ import javax.persistence.Table;
 public class AplicacionesEntity {
 	
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name = "id_app")
 	private Integer id_app;
 	
